4c330b16a9ab05cdc84c9a24cd0647bbb678da7f
Lamba237 <simontanyi2004@gmail.com>
2025-07-30 20:13:16 +0100

    Responsive Login page created using React and css

--- frontend/my-app/src/components/login_form.jsx
+++ frontend/my-app/src/components/login_form.jsx
@@ -7,38 +7,45 @@ export default function Login_form() {
                 <img src="./src/assets/xpress_logo.png" alt="login form logo" className="form-logo-icon" />
 
                 <div className="form-logo-text">
-                    <h3 className="login_form_header">Log in to your account</h3>
+                    <p className="login_form_header">Log in to your account</p>
                     <p className="login_form_text">Welcome back! Please enter your details.</p>
+                    
                 </div>
             </div>
 
             <form className="login-form">
                 <div className="form-group">
-                    <div className="email-field">
-                        <label htmlFor="email">Email</label><br />
-                        <input type="email" placeholder="Enter your email" /><br />
+                    <div className="email-field fields">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder="Enter your email" className="input" /> <br /> <br />
                     </div>
 
-                    <div className="password-field">
-                        <label htmlFor="password">Password</label><br />
-                        <input type="password" />
+                    <div className="password-field fields">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="password input"/> <br />
                     </div>
 
                     <div className="checkbox-field">
-                        <input type="checkbox" className="checkbox" />
-                        <label htmlFor="remember-me">Remember for 30 days</label>
+                        <div className="checkbox">
+                            <input type="checkbox" className="checkbox" />
+                            <label htmlFor="remember-me remember-me">Remember for 30 days</label>
+                        </div>
 
-                        <div className="forgot-password">
+                        <div className="forgot-password label">
                             Forgot password
                         </div>
                     </div>
                     
                     <div className="button-container">
-                        <button type="submit">Sign in</button><br />
-                        <button className="login-with-google">
+                        <button type="submit" className="submit_btn">Sign in</button><br />
+                        <button className="login-with-google input">
                             <img src="./src/assets/Social_icon.jpg" alt="Google icon" />
                             Sign in with Google
                         </button>
+                    </div>
+                    <div className="signup-link">
+                        <p>Don’t have an account?</p>
+                        <p>sign up</p>
                     </div>
                 </div>
             </form>
